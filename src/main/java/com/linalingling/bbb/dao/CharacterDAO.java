e1030044433a5c2509df7587b67d89fefc6c96be
linalingling <mysweet0520@gmail.com>
2026-05-11 12:37:47 +0800

    feat:implement Character entity and CharacterDAO with JDBC

--- src/main/java/com/linalingling/bbb/dao/CharacterDAO.java
+++ src/main/java/com/linalingling/bbb/dao/CharacterDAO.java
@@ -1,4 +1,44 @@
 package com.linalingling.bbb.dao;
+import com.linalingling.bbb.entity.Character;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CharacterDAO {
+    private Connection connection;
+
+    public CharacterDAO(Connection connection) {
+        this.connection = connection;
+    }
+    public Character findById(int id) throws SQLException {
+        //SQL
+        String sql = "SELECT * FROM characters WHERE id = ?";
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        pstmt.setInt(1,id);
+        ResultSet rs = pstmt.executeQuery();
+        if(rs.next()){
+            Character character = new Character();
+            character.setId(rs.getInt("id"));
+            character.setCharName(rs.getString("char_name"));
+            character.setBonusDecimal(rs.getBigDecimal("bonus_decimal"));
+            character.setTalentType(rs.getString("talent_type"));
+            return character;
+        }
+        return null; //暫存
+    }
+    public int insert(Character character) throws SQLException {
+        String sql = "INSERT INTO characters (char_name, bonus_decimal, talent_type) VALUES(?,?,?)";
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        pstmt.setString(1,character.getCharName());
+        pstmt.setBigDecimal(2,character.getBonusDecimal());
+        pstmt.setString(3,character.getTalentType());
+
+        return pstmt.executeUpdate();
+
+        // 之後寫insert into
+        return 0;
+    }
 }
+
--- src/main/java/com/linalingling/bbb/entity/Character.java
+++ src/main/java/com/linalingling/bbb/entity/Character.java
@@ -1,4 +1,48 @@
 package com.linalingling.bbb.entity;
+import java.math.BigDecimal;
 
 public class Character {
+    private int id;
+    private String charName;
+    private BigDecimal bonusDecimal;
+    private String talentType;
+
+    public Character(int id, String charName, BigDecimal bonusDecimal, String talentType) {
+        this.id = id;
+        this.charName = charName;
+        this.bonusDecimal = bonusDecimal;
+        this.talentType = talentType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCharName() {
+        return charName;
+    }
+
+    public void setCharName(String charName) {
+        this.charName = charName;
+    }
+
+    public BigDecimal getBonusDecimal() {
+        return bonusDecimal;
+    }
+
+    public void setBonusDecimal(BigDecimal bonusDecimal) {
+        this.bonusDecimal = bonusDecimal;
+    }
+
+    public String getTalentType() {
+        return talentType;
+    }
+
+    public void setTalentType(String talentType) {
+        this.talentType = talentType;
+    }
 }
